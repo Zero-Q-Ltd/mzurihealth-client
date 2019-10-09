af3890e7b3fe5aebc046338dd0e5f0cb1d5a4815
Kisinga <kamana@kisinga.family>
2019-10-09 14:22:37 +0300

    moved components to root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,18 +16,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { FuseSharedModule } from '../@fuse/shared.module';
 import { Error404Module } from './errorpages/404/error-404.module';
 import { Error500Module } from './errorpages/500/error-500.module';
-import { FrontendModule } from './frontend/frontend.module';
 import { AgmCoreModule } from '@agm/core';
 import { CommonModule } from '@angular/common';
 import { NotificationComponent } from './shared/components/notification/notification.component';
 import { Angulartics2Module } from 'angulartics2';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxStronglyTypedFormsModule } from 'ngx-strongly-typed-forms';
+import { AdminRoutingModule } from './admin/admin-routing.module';
+import { LayoutModule } from '@angular/cdk/layout';
+import { DocumentationModule } from './admin/documentation/documentation.module';
+import { AuthenticationModule } from './admin/authentication/authentication.module';
+import { CalendarModule } from 'angular-calendar';
+import { AdminSharedModule } from './admin/shared/admin-shared.module';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AdminlayoutComponent } from './admin/adminlayout.component';
+import { AdminprofileComponent } from './admin/adminprofile/adminprofile.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NotificationComponent,
+        DashboardComponent,
+        AdminlayoutComponent,
+        AdminprofileComponent,
     ],
     imports: [
         BrowserModule,
@@ -48,12 +61,25 @@ import { environment } from '../environments/environment';
         MatSnackBarModule,
         Error404Module,
         Error500Module,
-        FrontendModule,
         /* Angulartics2Module.forRoot(<Angulartics2Settings>{
              pageTracking: {clearHash: true, clearQueryParams: true},
              ga: {transport: 'beacon'},
              developerMode: !environment.gaTrackingId, // developerMode disables tracking
          }),*/
+
+        CommonModule,
+
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgxStronglyTypedFormsModule,
+        AdminRoutingModule,
+        LayoutModule,
+        DocumentationModule,
+        AuthenticationModule,
+        CalendarModule,
+
+        AdminSharedModule,
         Angulartics2Module.forRoot(),
         ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
 

7135c283d991490bf65b0582de435f82ffc4be9a
Kisinga <kamana@kisinga.family>
2019-12-07 17:28:18 +0300

    updated stitch rules

--- src/app/models/procedure/CustomProcedure.ts
+++ src/app/models/procedure/CustomProcedure.ts
@@ -18,6 +18,9 @@ export interface CustomProcedureConfig {
     _id: BSON.ObjectId;
     metadata: Metadata;
     hospitalId: BSON.ObjectId;
+    /**
+     * These are stored in the same document as they are often needed together and rarely change
+     */
     procedures: Array<CustomProcedure>;
 }
 export const emptycustomprocedure: CustomProcedure = {
